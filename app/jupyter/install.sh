80f62fbee0d4af7d300a6adb009669babf5b7f1f
everdistant <everdistant@gmail.com>
2021-02-16 23:00:37 -0500

    jupyter settings

--- app/jupyter/install.sh
+++ app/jupyter/install.sh
@@ -7,7 +7,9 @@ else
 	HOM="$1"
 fi
 
+jupyter lab --generate-config
+cp jupyter_notebook_config.py "$HOM"/.jupyter/
+
 jupyter qtconsole --generate-config
 cp jupyter_qtconsole_config.py "$HOM"/.jupyter/
-cp jupyter_notebook_config.py "$HOM"/.jupyter/
 cp .condarc "$HOM"
